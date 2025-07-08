363a1e6f31f61926d1c926dc687dedd68d07ace3
Sneha Hegde <snehabellemadky@gmail.com>
2025-07-08 22:54:17 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="forest",
+    version="0.0.1",
+    author="Sneha",
+    author_email="snehabellemadky@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
